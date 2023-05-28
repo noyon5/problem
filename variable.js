d71825de3135830bd2ff9b2cbe0194506c070b67
noyon5 <irnoyon858@gmail.com>
2023-05-28 21:45:24 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,15 @@
+var mathematics = 75.25; 
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+ var totalMarks = mathematics + biology + chemistry + physics + bangla ;
+ var averageMarks = totalMarks / 5;
+ console.log(averageMarks.toFixed(2));
+
+ var oranges = 350;
+ var apples = 350;
+ var givenMoney = 1000;
+ var totalExp = oranges + apples;
+ var getBack = givenMoney - totalExp;
+ console.log(getBack);
